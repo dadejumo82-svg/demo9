34588481024283afcbe84adfec09595c946f75ea
David Ade <dadejumo82@gmail.com>
2025-08-19 14:07:49 +0100

    version 2

---
+++ script.js
@@ -0,0 +1,72 @@
+//console.log('Hello');
+//console.log('I like pizza');
+//window.alert('This ia an alert');
+//window.alert('I like pizza');
+
+
+
+
+//document.getElementById('MY h').textContent ='hello';
+//document.getElementById('My p').textContent='I like pizza';
+//comment 
+
+/* this is a comment*/
+
+//variable = a container that stores a value,
+           // behaves as if it were the value it contains
+
+           //1. declaration let x;
+           //2. assignment x= 100
+
+    ///let x = 25;
+    //let price =10.99;
+    //let gpa =2.1;
+
+    //let firstName = "David";
+    //console.log(typeof firstName)
+    //console.log(` your gpa is; ${gpa}`);
+    //console.log(`you are ${x} years old`);
+
+
+    //let online = true;
+    //console.log(typeof online)
+    //console.log(`David is online: ${online}`)
+
+
+   // let fullName = "David";
+    //let age = 25;
+    //let isstudent = false;
+
+    //document. getElementById("p1").textContent = `your name is ${fullName}`;
+    //document. getElementById("p2").textContent = `you are ${age} years old`;
+    //document. getElementById("p3").textContent = `Enrolled: ${isstudent}`;
+
+    /* operator precedence
+
+        1. parenthesis()
+        2. exponents
+        3. mul, div, modulo(%)
+        4. addition, subtraction; */
+
+    //let result = 1 + 2 * 3 + 4 ** 2  
+    //console.log(result);
+
+
+       /* let username;
+        username = window.prompt (`whats your username`);
+
+        console.log(username);
+        */
+
+
+            //let username = "David";
+
+
+        //document.getElementById("mySubmit").onclick = function(){
+           // username = document.getElementById("myText").Value;
+         // console.log(username);
+
+        
+
+          
+
